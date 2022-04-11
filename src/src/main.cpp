47ab2211b62c53b38c82a5ef3135112becbff3bb
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-04-11 18:46:23 -0400

    Setup main.cpp
    Setup main.cpp for Writeing code

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello world!" << std::endl;
+	//std::cout << "Hello world!" << std::endl;
+
+
+
+
+
 }
