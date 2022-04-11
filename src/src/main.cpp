973513d00d2fd05bd76c4f897b3243cb82baf375
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-04-11 19:12:56 -0400

    Formated Code
    Formated Code

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -3,38 +3,28 @@
 
 */
 
-
-
-
-
 #include <iostream>
 
 using namespace std;
 
-
 static void show_usage(std::string name)
 {
-    std::cerr << "Usage: "  << " <option(s)> SOURCES"
+    std::cerr << "Usage: "
+              << " <option(s)> SOURCES"
               << "Options:\n"
               << "\t-h,--help\t\tShow this help message\n"
               << "\t-d,--destination DESTINATION\tSpecify the destination path"
               << std::endl;
 }
 
-
 int main(int argc, char *argv[])
 {
-	//std::cout << "Hello world!" << std::endl;
+    // std::cout << "Hello world!" << std::endl;
 
-if (argv[1] != "download") {
+    if (argv[1] != "download")
+    {
 
-	string LinkToDownload = argv[2];
-cout << "Downloading"<< LinkToDownload << std::endl;
-
-}
-
-
-
-
-
+        string LinkToDownload = argv[2];
+        cout << "Downloading" << LinkToDownload << std::endl;
+    }
 }
