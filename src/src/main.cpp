c9faba0de723348340be433cbdd42c49b70fdece
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-04-11 19:02:04 -0400

    Added Usage
    Added Usage

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -11,6 +11,17 @@
 
 using namespace std;
 
+
+static void show_usage(std::string name)
+{
+    std::cerr << "Usage: " << argv[0] << " <option(s)> SOURCES"
+              << "Options:\n"
+              << "\t-h,--help\t\tShow this help message\n"
+              << "\t-d,--destination DESTINATION\tSpecify the destination path"
+              << std::endl;
+}
+
+
 int main(int argc, char *argv[])
 {
 	//std::cout << "Hello world!" << std::endl;
