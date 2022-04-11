aa3961620c20084ebd81f7b3cda10854aab0f129
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-04-11 19:00:23 -0400

    Test 1
    Test 1

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,3 +1,12 @@
+/*
+
+
+*/
+
+
+
+
+
 #include <iostream>
 
 using namespace std;
@@ -5,6 +14,11 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	//std::cout << "Hello world!" << std::endl;
+
+if (argv[1] == "hi") {
+cout << "Hello world!" << std::endl;
+
+}
 
 
 
