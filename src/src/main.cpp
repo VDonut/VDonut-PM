30bbe6eb2d34ba8a89ad29b8950983a342d14785
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-04-11 19:05:55 -0400

    Removed Test 1
    Removed Test 1

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -26,7 +26,8 @@ int main(int argc, char *argv[])
 {
 	//std::cout << "Hello world!" << std::endl;
 
-if (argv[1] != "hi") {
+if (argv[1] != "download") {
+	LinkToDownload = 
 cout << "Hello world!" << std::endl;
 
 }
